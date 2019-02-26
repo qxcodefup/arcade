27da26217b81950a298a33723565e23946c159e4
david sena oliveira <sena.ufc@gmail.com>
2019-02-26 20:32:25 -0300

    up

--- update.py
+++ update.py
@@ -20,7 +20,7 @@ with open("./Readme.md", "w+") as saida:
     for i in range(1,len(base)):
         with open(base[i] + "/Readme.md", "r") as readme:
             texto = readme.readlines()
-            tags[re.compile(".*?((?:[a-z][a-z]+))", re.IGNORECASE|re.DOTALL).search(texto[0]).group(1)].append((("- [🞖](%s/Readme.md#qxcode) %s\n" % (base[i], texto[0].strip()[3:]))))
+            tags[re.compile(".*?((?:[a-z][a-z]+))", re.IGNORECASE|re.DOTALL).search(texto[0]).group(1)].append((("- [Link](%s/Readme.md#qxcode) %s\n" % (base[i], texto[0].strip()[3:]))))
 
     for tag in tags:
         tags[tag] = sorted(tags[tag], key=lambda x : int(re.compile(".*?(?:[a-z][a-z]*[0-9]+[a-z0-9]*).*?((?:[a-z][a-z]*[0-9]+[a-z0-9]*))",re.IGNORECASE|re.DOTALL).search(x).group(1)[1]))
