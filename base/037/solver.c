e6692a00dddb909762cefd20481a7c5b69ad2eb9
David Sena <sena.ufc@gmail.com>
2022-05-03 13:33:24 -0300

    updating

--- base/037/solver.c
+++ base/037/solver.c
@@ -1,32 +1,19 @@
 #include <stdio.h>
 
-int main(){
+int main() {
     int jog1 = 0;
     int jog2 = 0;
     scanf("%d %d", &jog1, &jog2);
     
-/* opcao 1
-    if(jog1 == jog2){
+    if (jog1 < jog2)
+        jog1 += 15; //coloco o 1 depois do 2 na lista circular
+
+    if (jog1 == jog2)
         puts("Empate");
-        return 0;
-    }
-    int dif = jog2 - jog1;
-    if(dif < 0)
-        dif += 15;
-    if(dif <= 7)
-        puts("Jogador 1")
-    else
+    else if (jog1 - jog2 <= 7)
         puts("Jogador 2");
-*/
-    
-    //opcao 2
-    if(jog1 == jog2)
-        puts("Empate");
-    else if(((jog2 - jog1) + 15) % 15 <= 7)
+    else
         puts("Jogador 1");
-    else
-        puts("Jogador 2");
-    
     return 0;
 }
 
