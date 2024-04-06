154a3512adb3c508acb584afb02567e4c82560a5
lion <sena.ufc@gmail.com>
2024-04-06 18:34:03 -0300

    fix: removendo link do css nos html

--- clone.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "Copying database..."
-cp Readme.md ../arcade/Readme.md
-rsync -au --delete base/ ../arcade/base
--- update.sh
+++ update.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-hooks="find base -maxdepth 2 -name Readme.md -print0 | sort -zn | xargs -0 -I '{}' dirname '{}'"
 
-# replacing Readme.md with .cache/Readme.md
-# replacing Readme.md with .cache/Readme.md on arcade
-eval $hooks | xargs -L 1 bash -c 'cd "$0" && cp .cache/Readme.md .'
+echo "Copying database..."
+cp ../ghost/Readme.md Readme.md
+rsync -au --delete ../ghost/base/ base
